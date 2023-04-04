c52982ebc68caa4b76592c7ef91554d96587e59c
Soheil Gharatappeh <soheilred@yahoo.com>
2023-04-04 09:38:39 -0400

    debugging

--- scripts/voc_annotation.py
+++ scripts/voc_annotation.py
@@ -12,8 +12,7 @@ def convert_voc_annotation(data_path, data_type, anno_path, use_difficult_bbox=F
     img_inds_file = os.path.join(data_path, 'ImageSets', 'Main', data_type + '.txt')
     with open(img_inds_file, 'r') as f:
         txt = f.readlines()
-        import ipdb; ipdb.set_trace()
-        image_inds = [line.strip() for line in txt]
+        image_inds = [line.split()[0] for line in txt]
 
     with open(anno_path, 'a') as f:
         for image_ind in image_inds:
